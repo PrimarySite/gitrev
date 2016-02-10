59157c2296d959e838d844040a988070a153e184
Mike Hurt <amusikal@yahoo.co.uk>
2016-02-10 12:47:34 +0000

    Update setup.py
    Fix semver.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='PrimarySite gitrev',
-    version='0.1dev',
+    version='0.1dev0',
     packages=['gitrev'],
     license='[TBA]',
     long_description=open('README.rst').read(),
