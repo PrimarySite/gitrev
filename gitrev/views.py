451d3edc3bf9856fbd0a9e8717bb682b1bf3dd72
Mike Hurt <mike.hurt@primarysite.net>
2016-02-10 12:15:39 +0000

    Clean up RevisionView.

--- gitrev/views.py
+++ gitrev/views.py
@@ -3,15 +3,14 @@
 from rest_framework import views
 from rest_framework.response import Response
 
+from . import __version__
+
 
 class RevisionView(views.APIView):
     http_method_names = ['get', ]
 
     def get(self, request):
         data = {}
-        context = self.get_context_data(self.kwargs)
-        print('>>>>>>>>>>>>>>>', context['appversion'])
-        appversion = getattr(request, 'appversion', None)
         if appversion:
-            data['version'] = appversion
+            data['version'] = __version__
         return Response(data)
