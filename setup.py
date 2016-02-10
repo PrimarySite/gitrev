1954117ff8866ca08a2313b649fab800f7032f7b
Mike Hurt <mike.hurt@primarysite.net>
2016-02-10 12:08:36 +0000

    Add simple setup.py.

---
+++ setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='PrimarySite gitrev',
+    version='0.1dev',
+    packages=['gitrev'],
+    license='[TBA]',
+    long_description=open('README.rst').read(),
+)
