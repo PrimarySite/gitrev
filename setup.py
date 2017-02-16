5cf7e3a94575517e608f46519863fc471b4ac0e9
Mike Hurt <amusikal@yahoo.co.uk>
2017-02-16 09:53:19 +0000

    Bump version to 0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='PrimarySite gitrev',
-    version='0.1dev0',
+    version='0.2',
     packages=['gitrev'],
     license='[TBA]',
     long_description=open('README.rst').read(),
