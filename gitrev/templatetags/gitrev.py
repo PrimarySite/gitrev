7f486ce549ac1b07b7c3123615f73f4a864b65af
Mike Hurt <amusikal@yahoo.co.uk>
2017-02-16 09:51:30 +0000

    Add templatetag and admin/base_site.html override

---
+++ gitrev/templatetags/gitrev.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Standard library.
+from django import template
+
+# Local
+from ..version import get_git_revision_string
+
+register = template.Library()
+
+@register.simple_tag
+def gitversion():
+    print('foooo')
+    if not gitversion.version:
+        gitversion.version = get_git_revision_string()
+    return gitversion.version
+gitversion.version = None
